49985f9ec79b6c30196739cf12144c81bc004d04
Benjamin Kostreva <benkostr@gmail.com>
2021-03-03 10:05:52 -0700

    feat: add ETHmxRewards

---
+++ contracts/rewards/ETHmxRewards.sol
@@ -0,0 +1,312 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
+import "../tokens/interfaces/IETHmx.sol";
+import "./interfaces/IETHmxRewards.sol";
+
+contract ETHmxRewards is Ownable, Pausable, IETHmxRewards {
+	using SafeERC20 for IERC20;
+	using SafeMath for uint256;
+
+	/* Immutable Public State */
+
+	address public immutable override ethmxAddr;
+	address public immutable override wethAddr;
+
+	/* Mutable Internal State */
+
+	uint256 internal _accruedRewardsPerToken;
+	mapping(address => uint256) internal _accruedRewardsPerTokenLast;
+	uint256 internal _lastTotalRewardsAccrued;
+	mapping(address => uint256) internal _rewardsFor;
+	mapping(address => uint256) internal _stakedFor;
+	uint256 internal _totalRewardsRedeemed;
+	uint256 internal _totalStaked;
+
+	/* Immutable Internal State */
+
+	uint256 internal constant _MULTIPLIER = 1e36;
+
+	/* Constructor */
+
+	constructor(address ethmxAddr_, address wethAddr_) Ownable() {
+		ethmxAddr = ethmxAddr_;
+		wethAddr = wethAddr_;
+	}
+
+	/* Public Views */
+
+	function accruedRewardsPerToken() public view override returns (uint256) {
+		return _accruedRewardsPerToken;
+	}
+
+	function accruedRewardsPerTokenLast(address account)
+		public
+		view
+		override
+		returns (uint256)
+	{
+		return _accruedRewardsPerTokenLast[account];
+	}
+
+	function lastTotalRewardsAccrued() public view override returns (uint256) {
+		return _lastTotalRewardsAccrued;
+	}
+
+	function rewardsBalanceOf(address account)
+		public
+		view
+		override
+		returns (uint256)
+	{
+		return _rewardsFor[account];
+	}
+
+	function stakedBalanceOf(address account)
+		public
+		view
+		override
+		returns (uint256)
+	{
+		return _stakedFor[account];
+	}
+
+	function totalRewardsAccrued() public view override returns (uint256) {
+		// Overflow is OK
+		return _currentRewardsBalance() + _totalRewardsRedeemed;
+	}
+
+	function totalRewardsRedeemed() public view override returns (uint256) {
+		return _totalRewardsRedeemed;
+	}
+
+	function totalStaked() public view override returns (uint256) {
+		return _totalStaked;
+	}
+
+	function unredeemableRewards() public view override returns (uint256) {
+		return _rewardsFor[address(0)];
+	}
+
+	/* Public Mutators */
+
+	function exit() public override {
+		address account = _msgSender();
+		unstakeAll();
+		_redeemReward(account, _rewardsFor[account]);
+	}
+
+	function pause() public override onlyOwner {
+		_pause();
+	}
+
+	function recoverUnredeemableRewards(address to, uint256 amount)
+		public
+		override
+		onlyOwner
+	{
+		require(
+			amount <= _rewardsFor[address(0)],
+			"ETHmxRewards: recovery amount greater than unredeemable"
+		);
+		_rewardsFor[address(0)] -= amount;
+		IERC20(wethAddr).safeTransfer(to, amount);
+		emit RecoveredUnredeemableRewards(_msgSender(), to, amount);
+	}
+
+	function recoverUnstaked(address to, uint256 amount)
+		public
+		override
+		onlyOwner
+	{
+		uint256 unstaked =
+			IERC20(ethmxAddr).balanceOf(address(this)).sub(_totalStaked);
+
+		require(
+			amount <= unstaked,
+			"ETHmxRewards: recovery amount greater than unstaked"
+		);
+
+		IERC20(ethmxAddr).safeTransfer(to, amount);
+		emit RecoveredUnstaked(_msgSender(), to, amount);
+	}
+
+	function recoverUnsupportedERC20(
+		address token,
+		address to,
+		uint256 amount
+	) public override onlyOwner {
+		require(token != ethmxAddr, "ETHmxRewards: cannot recover ETHmx");
+		require(token != wethAddr, "ETHmxRewards: cannot recover WETH");
+		IERC20(token).safeTransfer(to, amount);
+		emit RecoveredUnsupported(_msgSender(), token, to, amount);
+	}
+
+	function redeemAllRewards() public override {
+		address account = _msgSender();
+		_updateRewardFor(account);
+		_redeemReward(account, _rewardsFor[account]);
+	}
+
+	function redeemReward(uint256 amount) public override {
+		address account = _msgSender();
+		// Update reward first (since it only goes up)
+		_updateRewardFor(account);
+		require(
+			amount <= _rewardsFor[account],
+			"ETHmxRewards: cannot redeem more rewards than earned"
+		);
+		_redeemReward(account, amount);
+	}
+
+	function stake(uint256 amount) public override whenNotPaused {
+		require(amount != 0, "ETHmxRewards: cannot stake zero");
+
+		address account = _msgSender();
+		_updateRewardFor(account);
+		IERC20(ethmxAddr).safeTransferFrom(account, address(this), amount);
+
+		_stakedFor[account] = _stakedFor[account].add(amount);
+		_totalStaked = _totalStaked.add(amount);
+
+		emit Staked(account, amount);
+	}
+
+	function unpause() public override onlyOwner {
+		_unpause();
+	}
+
+	function unstake(uint256 amount) public override {
+		require(amount != 0, "ETHmxRewards: cannot unstake zero");
+		address account = _msgSender();
+
+		// Check against initial stake (since it only goes down)
+		require(
+			amount <= _stakedFor[account],
+			"ETHmxRewards: cannot unstake more than staked balance"
+		);
+
+		// Update stake
+		_updateRewardFor(account);
+		// Cap amount with updated stake
+		uint256 staked = _stakedFor[account];
+		if (amount > staked) {
+			amount = staked;
+		}
+
+		_unstake(account, amount);
+	}
+
+	function unstakeAll() public override {
+		address account = _msgSender();
+		// Update stake first
+		_updateRewardFor(account);
+		_unstake(account, _stakedFor[account]);
+	}
+
+	function updateAccrual() public override {
+		uint256 rewardsAccrued = totalRewardsAccrued();
+		// Overflow is OK
+		uint256 newRewards = rewardsAccrued - _lastTotalRewardsAccrued;
+
+		if (newRewards == 0) {
+			return;
+		}
+
+		if (newRewards < _totalStaked) {
+			// Add breathing room for better rounding, overflow is OK
+			_accruedRewardsPerToken += newRewards.mul(_MULTIPLIER) / _totalStaked;
+			_burnETHmx(newRewards);
+		} else {
+			uint256 leftover = newRewards - _totalStaked;
+			// Assign excess to zero address
+			_rewardsFor[address(0)] = _rewardsFor[address(0)].add(leftover);
+
+			if (_totalStaked != 0) {
+				// newRewards for tokens == totalStaked
+				_accruedRewardsPerToken += _MULTIPLIER;
+				_burnETHmx(_totalStaked);
+			}
+		}
+
+		_lastTotalRewardsAccrued = rewardsAccrued;
+	}
+
+	function updateReward() public override {
+		_updateRewardFor(_msgSender());
+	}
+
+	/* Internal Views */
+
+	function _currentRewardsBalance() internal view returns (uint256) {
+		return IERC20(wethAddr).balanceOf(address(this));
+	}
+
+	/* Internal Mutators */
+
+	function _burnETHmx(uint256 amount) internal {
+		_totalStaked = _totalStaked.sub(amount);
+		IETHmx(ethmxAddr).burn(amount);
+	}
+
+	function _redeemReward(address account, uint256 amount) internal {
+		// Should be guaranteed safe by caller (gas savings)
+		_rewardsFor[account] -= amount;
+		// Overflow is OK
+		_totalRewardsRedeemed += amount;
+
+		IERC20(wethAddr).safeTransfer(account, amount);
+
+		emit RewardPaid(account, amount);
+	}
+
+	function _unstake(address account, uint256 amount) internal {
+		if (amount == 0) {
+			return;
+		}
+
+		// Should be guaranteed safe by caller
+		_stakedFor[account] -= amount;
+		_totalStaked = _totalStaked.sub(amount);
+
+		IERC20(ethmxAddr).safeTransfer(account, amount);
+		emit Unstaked(account, amount);
+	}
+
+	function _updateRewardFor(address account) internal {
+		uint256 arpt = _accruedRewardsPerToken;
+		uint256 arptDelta = arpt - _accruedRewardsPerTokenLast[account];
+		uint256 staked = _stakedFor[account];
+
+		_accruedRewardsPerTokenLast[account] = arpt;
+
+		if (staked == 0 || arptDelta == 0) {
+			return;
+		}
+
+		// Calculate reward and new stake
+		uint256 newRewards = 0;
+		if (arptDelta >= _MULTIPLIER) {
+			// TODO add excess to some pendingRewards variable to enable redemption again. I don't think I need that, since totalStaked always reduces with rewards.
+			newRewards = staked;
+			staked = 0;
+		} else {
+			// The arm above should handle any plausible overflow
+			newRewards = staked.mul(arptDelta) / _MULTIPLIER;
+			uint256 lowStake = staked - newRewards;
+			uint256 lowRewards = (lowStake * arptDelta) / _MULTIPLIER;
+			newRewards = lowRewards + ((newRewards - lowRewards) / 2);
+			staked -= newRewards;
+		}
+
+		// Update state
+		_stakedFor[account] = staked;
+		_rewardsFor[account] = _rewardsFor[account].add(newRewards);
+	}
+}
---
+++ contracts/rewards/interfaces/IETHmxRewards.sol
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+interface IETHmxRewards {
+	/* Views */
+
+	function accruedRewardsPerToken() external view returns (uint256);
+
+	function accruedRewardsPerTokenLast(address account)
+		external
+		view
+		returns (uint256);
+
+	function ethmxAddr() external view returns (address);
+
+	function lastTotalRewardsAccrued() external view returns (uint256);
+
+	function rewardsBalanceOf(address account) external view returns (uint256);
+
+	function stakedBalanceOf(address account) external view returns (uint256);
+
+	function totalRewardsAccrued() external view returns (uint256);
+
+	function totalRewardsRedeemed() external view returns (uint256);
+
+	function totalStaked() external view returns (uint256);
+
+	function unredeemableRewards() external view returns (uint256);
+
+	function wethAddr() external view returns (address);
+
+	/* Mutators */
+
+	function exit() external;
+
+	function pause() external;
+
+	function recoverUnredeemableRewards(address to, uint256 amount) external;
+
+	function recoverUnstaked(address to, uint256 amount) external;
+
+	function recoverUnsupportedERC20(
+		address token,
+		address to,
+		uint256 amount
+	) external;
+
+	function redeemAllRewards() external;
+
+	function redeemReward(uint256 amount) external;
+
+	function stake(uint256 amount) external;
+
+	function unpause() external;
+
+	function unstake(uint256 amount) external;
+
+	function unstakeAll() external;
+
+	function updateAccrual() external;
+
+	function updateReward() external;
+
+	/* Events */
+
+	event RecoveredUnredeemableRewards(
+		address indexed author,
+		address indexed to,
+		uint256 amount
+	);
+	event RecoveredUnstaked(
+		address indexed author,
+		address indexed to,
+		uint256 amount
+	);
+	event RecoveredUnsupported(
+		address indexed author,
+		address indexed token,
+		address indexed to,
+		uint256 amount
+	);
+	event RewardPaid(address indexed to, uint256 amount);
+	event Staked(address indexed account, uint256 amount);
+	event Unstaked(address indexed account, uint256 amount);
+}
