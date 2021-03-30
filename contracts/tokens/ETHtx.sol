ef3f8a031ecba885322b59ee4d8f61efa98a62d6
Benjamin Kostreva <benkostr@gmail.com>
2021-03-30 16:02:46 -0600

    perf: ETHtx remove unused imports

--- contracts/tokens/ETHtx.sol
+++ contracts/tokens/ETHtx.sol
@@ -1,23 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.0;
 
-import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Pausable.sol";
-import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
-import "@openzeppelin/contracts/math/SafeMath.sol";
 
 import "./ERC20TxFee.sol";
 import "./interfaces/IETHtx.sol";
-import "./interfaces/IWETH.sol";
-import "../oracles/interfaces/IGasPrice.sol";
 
 contract ETHtx is Ownable, Pausable, ERC20TxFee, IETHtx {
-	using Address for address payable;
-	using SafeERC20 for IERC20;
-	using SafeMath for uint128;
-	using SafeMath for uint256;
-
 	/* Mutable Internal State */
 	address internal _minter;
 
